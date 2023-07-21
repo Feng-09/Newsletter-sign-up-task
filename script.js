cdc89b7437c3047a335ec2ca63294ada1723eeb3
Oyewande Boluwatife <tifeng9@gmail.com>
2023-07-21 01:48:30 +0100

    Ready to commit

---
+++ script.js
@@ -0,0 +1,42 @@
+"use strict";
+
+let btn = document.getElementById("subscribe");
+let input = document.getElementById("mail");
+let valid = document.getElementById("popUp");
+let bg = document.getElementById("main");
+let dismiss = document.getElementById("dismissBtn");
+let article = document.getElementById("submain");
+let invalid = document.getElementById("invalid");
+
+btn.addEventListener("click", () => {
+  let mail = document.getElementById("mail").value;
+  console.log(mail);
+  let validation1 = /\w@\w.\w/.test(mail);
+  console.log(validation1);
+  document.getElementById("iMail").innerHTML = mail;
+  if (validation1 === true) {
+    valid.classList.add("popup");
+    bg.classList.add("bg");
+    article.classList.add("invisible");
+  } else {
+    invalid.innerHTML = "Valid email required";
+    input.classList.add("fail");
+    invalid.classList.add("vibrate");
+  }
+});
+
+input.addEventListener("focus", () => {
+  document.getElementById("invalid").innerHTML = "";
+  invalid.classList.remove("vibrate");
+});
+
+dismiss.addEventListener("click", () => {
+  valid.classList.remove("popup");
+  bg.classList.remove("bg");
+  article.classList.remove("invisible");
+});
+
+bg.addEventListener("click", () => {
+  valid.classList.remove("popup");
+  bg.classList.remove("bg");
+});
